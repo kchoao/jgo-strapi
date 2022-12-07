09d698f92fb5f2e1046da6931b0937938a3fd98d
kchoao <kchoao@connect.ust.hk>
2022-12-07 18:28:14 +0800

    first commit

---
+++ src/admin/app.ts
@@ -0,0 +1,13 @@
+export default {
+  config: {
+    locales: [ 'en' ],
+    translations: {
+      en: {
+        'menus.customFields.field_name.label': 'Translated Label',
+        'menus.customFields.field_name.placeholder': 'Translated Placeholder',
+        'menus.customFields.field_name.description': 'Translated Description',
+      },
+    },
+  },
+  // etc.
+};
