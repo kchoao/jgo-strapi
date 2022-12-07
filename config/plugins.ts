255a6af5f3cc5bd33655a1f782a9630cc3198d1c
kchoao <kchoao@connect.ust.hk>
2022-12-07 18:34:39 +0800

    added aws-s3 plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,17 @@
-export default {
+export default ({ env }) => ({
+  upload: {
+    config: {
+        provider: 'aws-s3',
+        providerOptions: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+            params: {
+                Bucket: env('AWS_BUCKET_NAME'),
+            },
+        },
+    },
+  },
   menus: {
     config: {
       maxDepth: 3,
@@ -168,4 +181,4 @@ export default {
       },
     },
   },
-};
+});
