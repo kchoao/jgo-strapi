ffe14bf588a661e652e4d0e0be3bed6d6d925029
kchoao <kchoao@connect.ust.hk>
2022-12-09 19:02:28 +0800

    update database config

--- config/database.ts
+++ config/database.ts
@@ -6,7 +6,7 @@ export default ({ env }) => ({
       port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
-      // password: env('DATABASE_PASSWORD', 'admin'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL'),
     },
   },
