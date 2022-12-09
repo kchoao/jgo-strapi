21245c2ea51a6dd2e43f3f790b88202731a8fbf1
kchoao <kchoao@connect.ust.hk>
2022-12-09 16:32:48 +0800

    updated database config

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5433),
-      database: env('DATABASE_NAME', 'jgo-strapi-db'),
-      user: env('DATABASE_USERNAME', 'postgres'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
       // password: env('DATABASE_PASSWORD', 'admin'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
